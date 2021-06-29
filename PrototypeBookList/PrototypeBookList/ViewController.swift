10fa450f2fb0a053d5e7b4e2ef75e4f0f90887b9
john roque jorillo <jorillojohnroque@gmail.com>
2021-06-30 02:58:19 +0800

    Initian commit for prototype

---
+++ PrototypeBookList/PrototypeBookList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrototypeBookList
+//
+//  Created by John Roque Jorillo on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
