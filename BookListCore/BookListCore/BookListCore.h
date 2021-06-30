ea8a7b5dca5af0ecf60f3f83cd414f1e2400a66d
john roque jorillo <jorillojohnroque@gmail.com>
2021-07-01 03:49:52 +0800

    Add BooklistCore project

---
+++ BookListCore/BookListCore/BookListCore.h
@@ -0,0 +1,18 @@
+//
+//  BookListCore.h
+//  BookListCore
+//
+//  Created by John Roque Jorillo on 7/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookListCore.
+FOUNDATION_EXPORT double BookListCoreVersionNumber;
+
+//! Project version string for BookListCore.
+FOUNDATION_EXPORT const unsigned char BookListCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookListCore/PublicHeader.h>
+
+
